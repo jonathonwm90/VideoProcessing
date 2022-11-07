4d3fe976b1694243ea73d4439a57e6fb5a192a8c
jonathonwm90 <78045288+jonathonwm90@users.noreply.github.com>
2022-11-06 19:50:47 -0800

    re-commit of main code

--- final_vid_process.py
+++ final_vid_process.py
@@ -6,21 +6,21 @@ import cv2
 import sys
 from geometry import *
 import statistics as stc
-# this is atest of the reload
+
 plt.style.use('_mpl-gallery')
 
 def format_contour(x: int, contours: list):# gets the (x,y) coordinates for the desired contour, and plots it
+    
     formatted_contours = []
-    for i in range(len(contours[0])):
+    for i in range(len(contours[x][0])):
+        
         formatted_contours.append(contours[x][i][0])
     
     xvars, yvars = [], []
     for i in range(len(formatted_contours)):
-        np.array(formatted_contours)
         xvars.append(formatted_contours[i][0])
         yvars.append(formatted_contours[i][1])
-   
-    #plt.scatter(xvars, yvars, color="green", marker=".")
+
     return xvars, yvars
 
 def get_contour_center(y, contours_list, centers = [[],[]]): # gets the center of the desired contour number (in list of contours), y
@@ -39,34 +39,44 @@ def get_contour_center(y, contours_list, centers = [[],[]]): # gets the center o
     centers[0].append(x_ave)
     centers[1].append(y_ave)
 
-    #plt.plot(x_ave, y_ave, color="blue", marker="*" )
     return x_ave, y_ave, contour1
 
-def check_contours(x_range, y_range, contours):
+def check_contours(x_range, y_range, contours, previous_center): #y_range and x_range are the max distance the points can be from each other on respective axis
     centers = [[],[]]
-    x_ext, y_ext = []
+    good_centers = [[],[]]
+    contours_f = [[],[]]
     for i in range(len(contours)):
         x_cent, y_cent, contour = get_contour_center(i, contours)
+        contours_f[0].append(contour[0])
+        contours_f[1].append(contour[1])
         centers[0].append(x_cent)
         centers[1].append(y_cent)
-        x_ext.append([contour[0][0], contour[0][-1]])
-        y_ext.append([contour[1][0], contour[1,[-1]]])
-    
-    for i in range(len(centers[0])):
-        if (abs(centers[0][i] - x_ext[i][0])) < (x_range/2) and (abs(centers[0][i] - x_ext[i][1])) < (x_range/2):
-            pass
         
+    for i in range(len(centers[0])):
+        if (abs(centers[0][i] - previous_center[0])) < (x_range) and (abs(centers[1][i] - previous_center[1])) < (y_range):
+            good_centers[0].append(centers[0][i])
+            good_centers[1].append(centers[1][i])
 
-        if (abs(centers[1][i] - y_ext[i][0])) < (y_range/2) and (abs(centers[1][i] - y_ext[i][1])) < (y_range/2):
-            pass
-
+    if len(good_centers[0]) >= 2:
+        plt.plot(good_centers[0], good_centers[1], color="pink", marker="*")
+        plt.scatter(contours_f[0], contours_f[1], color="green", marker=".")
+        plt.imshow(start_frame)
+        plt.show()
+        print(good_centers)
+        raise Exception("two points were found in the constraints for the next point")
+    elif len(good_centers[0]) == 0 or len(good_centers[1]) == 0:
+        return 0, 0
+    else:
+        return good_centers[0][0], good_centers[1][0]
 
 
 def get_video_centers(video: str): #plays the video and returns centers and the capture object
     cap_centers = [[],[]]
+    bad_centers = [[],[]]
 
     cap = cv2.VideoCapture(video) #sys.argv[1]
     frames = 0
+    another = 0
 
     while (cap.isOpened()):
         ret, frame = cap.read()
@@ -75,24 +85,71 @@ def get_video_centers(video: str): #plays the video and returns centers and the 
                 plt.imshow(frame, alpha=.2)
         except:
             pass
-
+        
         if ret == True:
-
+            
         # Capture frame-by-frame
 
             blurred_image = cv2.GaussianBlur(frame, (7,7), 0)
             # read and blur the image
-
-            canny = cv2.Canny(blurred_image, 70, 420) 
+            
+            canny = cv2.Canny(blurred_image, 70, 275) #70, 420
             #                                 ^    ^
             #                 change these    |    |  to change contour finding
             contours, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            if len(contours) == 0 or len(contours) > 1:
+            #if len(contours) == 0 or len(contours) > 1:
+            #    continue
+            if len(contours) == 0:
                 continue
-            
-            get_contour_center(0, contours, cap_centers)
+            if frames == 0 or another == 1:
+                old_x = 0
+                old_y = 0
 
-            cv2.drawContours(frame, contours,  -1, (0,255,0), 2)
+                global start_frame
+                start_frame = frame
+
+                counter = 1
+
+                for i in contours:
+                    x, y, thing = get_contour_center(0, [i], bad_centers)
+                    print(counter, len(contours))
+                    if counter == len(contours):
+                        print("success")
+                        another = 1
+                        continue
+
+                    if abs(old_x - x) < 30:
+                        continue
+                    
+                    if abs(old_y - y) < 30:
+                        continue
+                    cv2.drawContours(blurred_image, contours, -1 ,(0,255,0), 2)
+                    cv2.imshow("objects Found", blurred_image)
+                    plt.imshow(frame)
+                    plt.plot(x, y, color="purple", marker=".")
+                    plt.show()
+                    answr = input("Is that the right center, y or n: ")
+                    
+                    old_x = x
+                    old_y = y
+                    counter += 1
+
+                    if answr == 'y':
+                        base_center = [x,y]
+                        another = 0
+                        break
+                    else:
+                        continue
+                    
+            if another == 0:
+                x_c, y_c = check_contours(50, 40, contours, base_center)
+
+                if x_c != 0 or y_c != 0: 
+                    cap_centers[0].append(x_c)
+                    cap_centers[1].append(y_c)
+
+            #get_contour_center(0, contours, cap_centers)
+            cv2.drawContours(frame, contours, -1 ,(0,255,0), 2)
             cv2.imshow("objects Found", frame)
 
             frames += 1
@@ -106,28 +163,31 @@ def get_video_centers(video: str): #plays the video and returns centers and the 
     return cap_centers, cap
 
 vid_centers, capture = get_video_centers(sys.argv[1])
-
+print(vid_centers)
 def sequence_find(x, points):
     RotationCenters = [[], []]
-
+    
     for i in range(0, len(points[0]) - (2 * x), x):
         
 
-        x1_1 = points[0][i]
-        y1_1 = points[1][i]
-        x1_2 = points[0][i+x]
-        y1_2 = points[1][i+x]
+        x1_1 = points[0][i][0]
+        y1_1 = points[1][i][0]
+        x1_2 = points[0][i+x][0]
+        y1_2 = points[1][i+x][0]
 
-        x2_1 = points[0][i+x]
-        y2_1 = points[1][i+x]
-        x2_2 = points[0][i+(2*x)]
-        y2_2 = points[1][i+(2*x)]
+        x2_1 = points[0][i+x][0]
+        y2_1 = points[1][i+x][0]
+        x2_2 = points[0][i+(2*x)][0]
+        y2_2 = points[1][i+(2*x)][0]
 
         line_1 = Line(coor1 = [x1_1, y1_1], coor2 = [x1_2, y1_2])
         line_2 = Line(coor1 = [x2_1, y2_1], coor2 = [x2_2, y2_2])
 
+        #print(line_1.y1, line_1.m, line_1.x1)
+
         line_1.get_m_b()
         line_2.get_m_b()
+
         if line_1.m == 0 or line_2.m == 0:
             continue
 
@@ -143,17 +203,23 @@ def sequence_find(x, points):
     return RotationCenters
 
 Cents_Rot = sequence_find(3, vid_centers)
+print(Cents_Rot)
 
 def find_ave_COR(used_cors = [[0,1],[0,1]]):
     for i in range(int(sys.argv[2])):
-        
         x_ave_cor = np.mean(used_cors[0])
-        SD_x_cor = stc.stdev(used_cors[0], xbar = x_ave_cor)
+        try:
+            SD_x_cor = stc.stdev(used_cors[0], xbar = x_ave_cor)
+        except:
+            continue
         SD_upp_thresh_x = (x_ave_cor + SD_x_cor)
         SD_low_thresh_x = (x_ave_cor - SD_x_cor)
 
         y_ave_cor = np.mean(used_cors[1])
-        SD_y_cor = stc.stdev(used_cors[1], xbar = y_ave_cor)
+        try:
+            SD_y_cor = stc.stdev(used_cors[1], xbar = y_ave_cor)
+        except:
+            continue
         SD_upp_thresh_y = (y_ave_cor + SD_y_cor)
         SD_low_thresh_y = (y_ave_cor - SD_y_cor)
 
